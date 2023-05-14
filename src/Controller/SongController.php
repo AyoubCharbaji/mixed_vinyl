e382ce9cb279616364c8e0dd4097dccd2dcb6681
Charbaji Ayoub <ayoub.charbaji@gmail.com>
2023-05-14 19:30:54 +0100

    Look mom! A real app

--- src/Controller/SongController.php
+++ src/Controller/SongController.php
@@ -3,9 +3,10 @@
 namespace App\Controller;
 
 use Psr\Log\LoggerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class SongController extends AbstractController
 {
